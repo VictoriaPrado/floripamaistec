5e92830f568d9c23bf5dec19af0314ad50ce4b09
Victoria <victoriaprado16@gmail.com>
2023-02-09 22:08:13 -0300

    adicionando os exercicios 4,3,5 da semana 1

---
+++ Modulo 01/S1/calculadora/calculadora/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Importação de bibliotecas
+
+// Nome da classe
+
+// Declaração método
+
+// Declaração de variáveis
+int opcao = 0, num1 = 0, num2 = 0;
+float resultado = 0;
+
+// Entradas
+Console.WriteLine("Escolha uma operação: \n\n" +
+    "1 - Soma\n2 - Subtração\n3 - Multriplicação\n4 - Divisão\n\n" +
+    "5 - Sair\n\nDigite a opção escolhida: ");
+
+opcao = int.Parse(Console.ReadLine());
+
+// Processamento
+switch (opcao)
+{
+    // bloco de instruções da estrutura condicional
+    case 1:
+        Console.WriteLine("Operação somar.");
+        Console.Write("Digite o número 1: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o número 2: ");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 + num2;
+        break;
+    case 2:
+        Console.WriteLine("Operação subtração.");
+        Console.Write("Digite o número 1: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o número 2: ");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 - num2;
+        break;
+    case 3:
+        Console.WriteLine("Operação multiplicação.");
+        Console.Write("Digite o número 1: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o número 2: ");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 * num2;
+        break;
+    case 4:
+        Console.WriteLine("Operação divisão.");
+        Console.Write("Digite o número 1: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o número 2: ");
+        num2 = int.Parse(Console.ReadLine());
+        resultado = num1 / num2;
+        break;
+    case 5:
+        Console.WriteLine("Saindo do sistema.");
+        break;
+}
+
+Console.WriteLine("Resultado da operação: " + resultado);
+
+// Saídas
