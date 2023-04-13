4a826bcdf7eefa55253ea48a8a098d36a95061e4
Victoria <victoriaprado16@gmail.com>
2023-04-12 21:22:40 -0300

    adicionado Ex 3 ao 6 da Semana 3

---
+++ Modulo 01/S3/Ex 3 ao 6/M1S03/DadosBancarios/Enum/TipoContaEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1S03.DadosBancarios.Enum
+{
+    public enum TipoContaEnum
+    {
+        CORRENTE = 0,
+        POUPANCA = 1
+    }
+}
