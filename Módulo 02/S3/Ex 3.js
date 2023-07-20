9c7b83abbbf1df53274c29214eae9ad71138ec1e
Victoria <victoriaprado16@gmail.com>
2023-07-20 01:05:16 -0300

    Adicionado Ex 03 Semana 03

---
+++ Módulo 02/S3/Ex 3.js
@@ -0,0 +1,22 @@
+
+function calcularIMC(peso, altura) {
+    var resultado = peso / (altura * altura)
+        if (resultado < 18.5){
+            console.log("O seu IMC é: " +resultado.toFixed(2) + ", está abaixo do recomendado para a sua altura.")
+        }
+        else if(resultado > 18.5 && resultado < 24.9){
+            console.log("O seu IMC é: " +resultado.toFixed(2) + ", é considerado normal para a sua altura.")
+        }
+        else if(resultado => 24.9 && resultado < 30){
+            console.log("O seu IMC é: " +resultado.toFixed(2) + ", está acima do recomendado para a sua altura.")
+        }
+        else if(resultado => 30 && resultado <86.7){
+            console.log("O seu IMC é: " +resultado.toFixed(2) + ", está acima do recomendado para a sua altura.")
+        }
+
+    return resultado.toFixed(2)
+}
+
+
+calcularIMC(80, 1.76)
+
